0a7937be4fe59fa60682cfb0161a4417d9a6ca8d
Yahir177 <99152114+Yahir177@users.noreply.github.com>
2022-02-07 15:34:02 -0600

    Add files via upload

---
+++ actividad2.js
@@ -0,0 +1,20 @@
+var mes = 10;
+
+if(mes >= 1 && mes <=2 ){
+    alert("Es invierno");
+  
+} else if(mes >=3 && mes <=5){
+    alert("Es Primavera");
+  
+} else if(mes >=6 && mes <=8){
+    alert("Es Verano");
+  
+} else if(mes >=9 && mes <=11){
+     alert('Es Otoño');
+
+  
+} else {
+  alert("El número ingresado no corresponde a ningun mes")
+
+}
+  alert( "Ari Yahir Ramirez Huertas, Canet: 22005143")
